25793905dacbbf59da1f1b1f4938ad8b8b35c654
geholzmann <georg.holzmann@aon.at>
2017-12-11 18:55:43 +0100

    ProductCatalogLocal - Init
    ProductCatalogeLocal - Service init

---
+++ SolHandelsplattform V.01/ProductCatalogeLocal/Models/ProductDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogeLocal.Models
+{
+    public class ProductDBContext : DbContext
+    {
+        public DbSet<Product> products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProductsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Products");
+            modelBuilder.Entity<product>().Property(a => a.Partei).IsRequired();
+        }
+    }
+}
---
+++ SolHandelsplattform V.01/ProductCatalogeLocal/Models/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogeLocal.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        static ProductRepository()
+        {
+            using (var ctx = new ProductDBContext())
+            {
+                ctx.Database.EnsureDeleted();
+                ctx.Database.EnsureCreated();
+                ctx.SaveChanges();
+            }
+        }
+
+        public List<Product> GetAll()
+        {
+            using (var ctx = new ProductDBContext())
+            {
+                return ctx.auftritte.OrderBy(a => a.Id).ToList();
+            }
+        }
+
+        
+    }
+}
