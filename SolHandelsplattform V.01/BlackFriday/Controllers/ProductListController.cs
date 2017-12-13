994bdd111483f0c8eea5d59d26b150eaedd1c9a7
Michael Marolt <michael.marolt@edu.campus02.at>
2017-12-13 18:36:46 +0100

    blackfr

--- SolHandelsplattform V.01/BlackFriday/Controllers/ProductListController.cs
+++ SolHandelsplattform V.01/BlackFriday/Controllers/ProductListController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,7 +18,11 @@ namespace BlackFriday.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Windows Phone", "BlackBerry" };
+            WebRequest request = WebRequest.Create("http://productcataloge-marolt.azurewebsites.net/api/Products/");
+            WebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string line = reader.ReadLine();
+            return JsonConvert.DeserializeObject<string[]>(line).ToList<string>();
         }
         
     }
